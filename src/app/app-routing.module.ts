3bfd8a69720ae029f03a784e6e3c1f407c688f4f
fazt <fazt@faztweb.com>
2019-12-13 15:37:31 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,32 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: "", redirectTo: "places", pathMatch: "full" },
+  {
+    path: "places",
+    children: [
+      {
+        path: "",
+        loadChildren: () =>
+          import("./places/places.module").then(m => m.PlacesPageModule)
+      },
+      {
+        path: ":placeId",
+        loadChildren: () =>
+          import("./places/place-detail/place-detail.module").then(
+            m => m.PlaceDetailPageModule
+          )
+      }
+    ]
+  },
+  {
+    path: "new-place",
+    loadChildren: () =>
+      import("./places/place-add/place-add.module").then(
+        m => m.PlaceAddPageModule
+      )
+  }
 ];
 
 @NgModule({
@@ -12,4 +35,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
